8437441564f8b1673c07497011779b1e35d55c80
maxbr <maxbr@mi.fu-berlin.de>
2016-09-21 17:51:57 +0200

    Add license headers
    JIRA: RELENG-12
    
    Change-Id: I6aeb82b2f72d086b70737f86282f8ed0c48f83cf
    Signed-off-by: maxbr <maxbr@mi.fu-berlin.de>

--- tools/pharos-dashboard/static/js/dataTables-sort.js
+++ tools/pharos-dashboard/static/js/dataTables-sort.js
@@ -1,3 +1,13 @@
+/*****************************************************************************
+* Copyright (c) 2016 Max Breitenfeldt and others.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution, and is available at
+* http://www.apache.org/licenses/LICENSE-2.0
+*****************************************************************************/
+
+
 /**
  * This is a sort function for dataTables to sort tables by the status column.
  * The order should be: online < online/idle < offline
