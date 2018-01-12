010b59e8302f65c1b857ee7cc36455dd9a2dc158
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-01-12 21:49:01 +0100

    [PDF] config: Add/update license headers
    This only covers the `config` subdir.
    
    Change-Id: I1fb9470e216cc2de9a4ac91de44d99e7044e708e
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- config/utils/check-jinja2.sh
+++ config/utils/check-jinja2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2016 Linux Foundation and others.
+# Copyright (c) 2018 Linux Foundation and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
