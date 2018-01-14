013d03843a81a5793966a88e3f504da3cfcb4809
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-01-14 02:26:05 +0100

    [IDF] Add IDF schema validation based on version
    - add basic structure for IDF schema validation;
    - fill in Fuel section;
    - add stub for Daisy section;
    - add stub for Compass section;
    - add stub for net_config;
    - enable IDF schema validation via `check-schema.sh`;
    - prepare for new IDF versions with different schemas,
      while allowing subsection schema reuse from IDF v0.1;
    
    Change-Id: I59b1c9d857e9dee9e6ff9956e81b2ae6dc27a767
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- config/utils/check-schema.sh
+++ config/utils/check-schema.sh
@@ -11,20 +11,33 @@ export PATH=$PATH:/usr/local/bin/
 
 VALIDATE_SCHEMA='./config/utils/validate_schema.py'
 PDF_SCHEMA='./config/pdf/pod1.schema.yaml'
+IDF_SCHEMA='./config/pdf/idf-pod1.schema.yaml'
 RC=0
 
+SUMMARY+=";;PDF;IDF;\n"
 while IFS= read -r lab_config; do
     pdf_cmd="${VALIDATE_SCHEMA} -s ${PDF_SCHEMA} -y ${lab_config}"
+    idf_cmd="${VALIDATE_SCHEMA} -s ${IDF_SCHEMA} -y ${lab_config/pod/idf-pod}"
     echo "###################### ${lab_config} ######################"
     pdf_out=$(${pdf_cmd} |& sed 's|ENC\[PKCS.*\][\\n]*|opnfv|g')
     if [ -z "${pdf_out}" ]; then
-        SUMMARY+=";${lab_config#labs/};OK;\n"
+        SUMMARY+=";${lab_config#labs/};OK;"
         echo "[PDF] [OK] ${pdf_cmd}"
     else
-        SUMMARY+=";${lab_config#labs/};ERROR;\n"
+        SUMMARY+=";${lab_config#labs/};ERROR;"
         RC=1
         echo "${pdf_out}"
         echo "[PDF] [ERROR] ${pdf_cmd}"
+    fi
+    if [ ! -f "${lab_config/pod/idf-pod}" ]; then
+        SUMMARY+="-;\n"
+    elif ${idf_cmd}; then
+        SUMMARY+="OK;\n"
+        echo "[IDF] [OK] ${idf_cmd}"
+    else
+        SUMMARY+="ERROR;\n"
+        RC=1
+        echo "[IDF] [ERROR] ${idf_cmd}"
     fi
     echo ''
 done < <(find 'labs' -name 'pod*.yaml')
